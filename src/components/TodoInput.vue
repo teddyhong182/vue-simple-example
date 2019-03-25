ca14a001c77d27565c2b6ef48aafe144eb4f3c47
Kiwon Hong <rusdlsla@gmail.com>
2019-03-25 13:49:12 +0900

    input complete.

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,57 @@
 <template>
-  <div>Input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "TodoInput"
+  export default {
+    data() {
+      return {
+        newTodoItem: ''
+      }
+    },
+    methods: {
+      clearInput: function () {
+        this.newTodoItem = '';
+      },
+      addTodo() {
+        if (this.newTodoItem !== "") {
+          let value = this.newTodoItem && thos.newTodoItem.trim();
+          localStorage.setItem(value, value);
+          this.clearInput();
+        }
+      }
     }
+  }
 </script>
 
 <style scoped>
-
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
